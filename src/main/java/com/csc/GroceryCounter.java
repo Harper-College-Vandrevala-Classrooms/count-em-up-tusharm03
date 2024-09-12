865ab9b7d5b4624a94776b079f33e33b24f003cc
tusharm03 <muppidi.tushar@gmail.com>
2024-09-12 14:36:53 -0500

    finished assignment

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,125 @@
 package com.csc;
 
 public class GroceryCounter {
-  // Put your code here!
+    private int count;        // Represents the counter value
+    private int overflows;    // Number of overflows
+    private int maxValue;     // Custom maximum counter value
+
+    // Constructor with a default maximum value of 9999
+    public GroceryCounter(int startingValue, int maxValue) {
+        if (startingValue < 0 || startingValue > maxValue) {
+            throw new IllegalArgumentException("Starting value must be within the range 0 to " + maxValue);
+        }
+        this.count = startingValue;
+        this.overflows = 0;
+        this.maxValue = maxValue;
+    }
+
+    // Constructor with a default maximum value of 9999
+    public GroceryCounter(int startingValue) {
+        this(startingValue, 9999);
+    }
+
+    // Default constructor with default starting value of 0 and maximum value of 9999
+    public GroceryCounter() {
+        this(0, 9999);
+    }
+
+    // Increment the counter 
+    private void incrementCounter() {
+        count++;
+        if (count > maxValue) {
+            count = 0;
+            overflows++;
+        }
+    }
+
+    // Increment the tens digit
+    public void incrementTens() {
+        count += 100;
+        while (count > maxValue) {
+            count -= (maxValue + 1); 
+            incrementCounter();
+        }
+    }
+
+    // Increment the ones digit
+    public void incrementOnes() {
+        count += 1;
+        while (count > maxValue) {
+            count -= (maxValue + 1); 
+            incrementCounter();
+        }
+    }
+
+    // Increment the tenths digit
+    public void incrementTenths() {
+        count += 10;
+        while (count > maxValue) {
+            count -= (maxValue + 1); 
+            incrementCounter();
+        }
+    }
+
+    // Increment the hundredths digit
+    public void incrementHundredths() {
+        count += 100;
+        while (count > maxValue) {
+            count -= (maxValue + 1); 
+            incrementCounter();
+        }
+    }
+
+    // Decrement the tens digit
+    public void decrementTens() {
+        count -= 100;
+        while (count < 0) {
+            count += (maxValue + 1); 
+            overflows--;
+        }
+    }
+
+    // Decrement the ones digit
+    public void decrementOnes() {
+        count -= 1;
+        while (count < 0) {
+            count += (maxValue + 1); 
+            overflows--;
+        }
+    }
+
+    // Decrement the tenths digit
+    public void decrementTenths() {
+        count -= 10;
+        while (count < 0) {
+            count += (maxValue + 1); 
+            overflows--;
+        }
+    }
+
+    // Decrement the hundredths digit
+    public void decrementHundredths() {
+        count -= 100;
+        while (count < 0) {
+            count += (maxValue + 1); 
+            overflows--;
+        }
+    }
+
+    // Format the current count 
+    public String total() {
+        int dollars = count / 100;
+        int cents = count % 100;
+        return String.format("$%02d.%02d", dollars, cents);
+    }
+
+    // Get the number of overflows
+    public int numberOfOverflows() {
+        return overflows;
+    }
+
+    public void clear() {
+        count = 0;
+        overflows = 0;
+    }
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -1,24 +1,103 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGroceryCounter {
 
-  GroceryCounter counter;
+    @Test
+    public void testInitialState() {
+        GroceryCounter counter = new GroceryCounter();
+        assertEquals(0, counter.numberOfOverflows(), "Initial overflows should be 0");
+        assertEquals("$00.00", counter.total(), "Initial total should be $00.00");
+    }
 
-  @BeforeEach
-  void setUp() {
-    counter = new GroceryCounter();
-  }
+    @Test
+    public void testCustomStartingValue() {
+        GroceryCounter counter = new GroceryCounter(1234);
+        assertEquals(0, counter.numberOfOverflows(), "Initial overflows should be 0");
+        assertEquals("$12.34", counter.total(), "Initial total should be $12.34");
+    }
 
-  // Put your tests here!
+    @Test
+    public void testCustomMaximumValue() {
+        GroceryCounter counter = new GroceryCounter(0, 19999);
+        counter.incrementTens(); // Increment by 100
+        assertEquals("$01.00", counter.total(), "Total after incrementing tens should be $01.00");
+        counter.incrementOnes(); // Increment by 1
+        assertEquals("$01.01", counter.total(), "Total after incrementing ones should be $01.01");
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    public void testIncrementTens() {
+        GroceryCounter counter = new GroceryCounter();
+        counter.incrementTens(); // Increment by 100
+        assertEquals("$01.00", counter.total(), "Total after incrementing tens by 100 should be $01.00");
+        counter.incrementTens(); // Increment by another 100
+        assertEquals("$02.00", counter.total(), "Total after incrementing tens by 200 should be $02.00");
+    }
 
+    @Test
+    public void testIncrementOnes() {
+        GroceryCounter counter = new GroceryCounter();
+        for (int i = 0; i < 5; i++) {
+            counter.incrementOnes(); // Increment by 1 each time
+        }
+        assertEquals("$00.05", counter.total(), "Total after incrementing ones by 5 should be $00.05");
+    }
+
+    @Test
+    public void testIncrementTenths() {
+        GroceryCounter counter = new GroceryCounter();
+        counter.incrementTenths(); // Increment by 10
+        assertEquals("$00.10", counter.total(), "Total after incrementing tenths by 10 should be $00.10");
+    }
+
+    @Test
+    public void testIncrementHundredths() {
+        GroceryCounter counter = new GroceryCounter();
+        counter.incrementHundredths(); // Increment by 100
+        assertEquals("$01.00", counter.total(), "Total after incrementing hundredths by 100 should be $01.00");
+    }
+
+    @Test
+    public void testDecrementTens() {
+        GroceryCounter counter = new GroceryCounter(1000);
+        counter.decrementTens(); // Decrement by 100
+        assertEquals("$09.00", counter.total(), "Total after decrementing tens by 100 should be $09.00");
+        counter.decrementTens(); // Decrement by another 100
+        assertEquals("$08.00", counter.total(), "Total after decrementing tens by 200 should be $08.00");
+    }
+
+    @Test
+    public void testDecrementOnes() {
+        GroceryCounter counter = new GroceryCounter(5);
+        counter.decrementOnes(); // Decrement by 1
+        assertEquals("$00.04", counter.total(), "Total after decrementing ones by 1 should be $00.04");
+    }
+
+    @Test
+    public void testDecrementTenths() {
+        GroceryCounter counter = new GroceryCounter(15);
+        counter.decrementTenths(); // Decrement by 10
+        assertEquals("$00.05", counter.total(), "Total after decrementing tenths by 10 should be $00.05");
+    }
+
+    @Test
+    public void testDecrementHundredths() {
+        GroceryCounter counter = new GroceryCounter(100);
+        counter.decrementHundredths(); // Decrement by 100
+        assertEquals("$00.00", counter.total(), "Total after decrementing hundredths by 100 should be $00.00");
+    }
+
+    @Test
+    public void testClear() {
+        GroceryCounter counter = new GroceryCounter();
+        for (int i = 0; i < 500; i++) {
+            counter.incrementOnes(); // Increment by 1 each time
+        }
+        counter.clear();
+        assertEquals(0, counter.numberOfOverflows(), "Overflows should be 0 after clearing");
+        assertEquals("$00.00", counter.total(), "Total should be $00.00 after clearing");
+    }
 }
